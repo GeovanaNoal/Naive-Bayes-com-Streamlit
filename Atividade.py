4ea57b32418385064ddf8c487ae7f0c9ea5b3e00
GeovanaNoal <116393239+GeovanaNoal@users.noreply.github.com>
2022-11-01 19:34:55 -0300

    Update Atividade.py

--- Atividade.py
+++ Atividade.py
@@ -13,8 +13,8 @@ dados = pd.read_csv('Iris_Floresta_Randomica.csv')
 
 classes = dados['Species'] 
 
-x = np.array([[1,2],[1,2],[1,2],[-2,0],[2,3],[-4,0],[-1,1],[1,2],[-2,2],[2,7],[-4,1],[0,0]])
-y = np.array([1, 2, 6, 7, 2, 9, 3, 13, 8, 10, 4,7 ])
+#x = np.array([[1,2],[1,2],[1,2],[-2,0],[2,3],[-4,0],[-1,1],[1,2],[-2,2],[2,7],[-4,1],[0,0]])
+#y = np.array([1, 2, 6, 7, 2, 9, 3, 13, 8, 10, 4,7 ])
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
