91c4961b6ea60ec0069c7426aa79da2bdbb98111
GeovanaNoal <116393239+GeovanaNoal@users.noreply.github.com>
2022-11-01 19:37:37 -0300

    Update Atividade.py

--- Atividade.py
+++ Atividade.py
@@ -19,7 +19,7 @@ classes = dados['Species']
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
 
-model = GaussianNB()
+model = NaveBayes()
 model.fit(x_train, y_train)
 teste = np.array([[2,7],[1,2],[-2,0],[-4,1]])
 predicted = model.predict(teste)
