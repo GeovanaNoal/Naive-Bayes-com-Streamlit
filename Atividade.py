bcbb477ff2801390fc57ebc6d9a16098341d9e31
GeovanaNoal <116393239+GeovanaNoal@users.noreply.github.com>
2022-11-04 18:17:08 -0300

    Update Atividade.py

--- Atividade.py
+++ Atividade.py
@@ -34,7 +34,7 @@ PetalWidthCm = st.number_input('Digite a largura da petala')
 if st.button('Clique aqui'):
   resultado = model.predict([[SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm]])
   
-  if resultado == 'Iris-setosa'
+  if resultado = 'Iris-setosa'
     st.image('iris_setosa.jpg)
   
   
